2a0d28741f5088a3ce0dc515049eee8f0cb829d9
Michal Skowronski <skowronskimichal@outlook.com>
2023-04-21 16:09:37 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script>
+import CreateTask from './components/CreateTask.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CreateTask,
+  },
+  data() {
+    return {
+      items: [],
+    }
+  },
+  methods: {
+    addItem(item) {
+      this.items.push(item)
+      console.log(this.items);
+    },
+
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1 class="appTitle">Todo App</h1>,
+    <CreateTask :items="items" @add-item="addItem" />
+    <ul><li v-for="item in items" style="color: blue">{{ item }}</li></ul>
+  </header>
+
+</template>
+
+<style scoped>
+
+header {
+  line-height: 1.5;
+  width: 60vw;
+  min-height: 60vh;
+  
+}
+
+@media (min-width: 512px) {
+  header {
+    display: flex;
+    flex-direction: column;
+    background-color: #fff;
+    place-items: top;
+    border-radius: 5px;
+  }
+
+  .appTitle {
+  margin-left: 10%;
+  padding-top: 2rem;
+  display: block;
+  color: black;
+}
+}
+</style>
+
+
---
+++ src/components/CreateTask.vue
@@ -0,0 +1,44 @@
+<script >
+// import App from '../App.vue';
+
+export default {
+  name:'TheWelcome',
+  props: ['items'],
+data() {
+  return {
+    insertedValue: '',
+  }
+},
+
+methods: {
+  addItem() {
+    this.$emit('add-item', this.insertedValue);
+  }
+}
+}
+</script>
+
+<template>
+  <div class="addComponent">
+    <input class="newTask" v-bind="insertedValue">
+    <button class="addBtn" type="submit" @click="addItem">Add</button>
+  </div>
+  
+</template>
+
+<style>
+
+.addComponent{
+  display: block;
+}
+
+.newTask {
+  box-sizing: border-box;
+  margin-left: 10%;
+}
+
+.addBtn {
+  display: inline-block;
+}
+
+</style>
